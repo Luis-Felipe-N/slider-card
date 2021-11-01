a46a5a76aad7d3e2be116870bb1c5fdcadf3d488
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-10-31 22:39:21 -0300

    fisrt commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card')
+const sliderCard = document.querySelector('slider-card')
+cards.forEach( card => console.log(card.getBoundingClientRect())) 
+
+const btnNext = document.querySelector('.btn-next')
+btnNext.onclick = () => {
+
+}
